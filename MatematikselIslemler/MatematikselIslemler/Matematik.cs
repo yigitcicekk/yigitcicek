b6da70fa0e1a46b1b6c3c6ac53e88084a58628f8
yigitcicekk <ygtcck.67@gmail.com>
2022-10-23 19:24:48 +0300

    MatematikselIslemler
    Kullanıcıdan belirtilen girişler doğrultusunda matematiksel işlemler yapar

---
+++ MatematikselIslemler/MatematikselIslemler/Matematik.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatematikselIslemler
+{
+    internal class Matematik
+    {
+        //Topla
+        public decimal toplamaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 + sayi2;
+            return sonuc;
+        }
+
+        // Çıkartma
+        public decimal cıkartmaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 - sayi2;
+            return sonuc;
+        }
+
+        // Bölme
+        public decimal bolmeIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 / sayi2;
+            return sonuc;
+        }
+
+        // Çarpma
+        public decimal carpmaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 * sayi2;
+            return sonuc;
+        }
+
+        public void menuHazirla()
+        {
+            Console.Clear();
+            Console.WriteLine("***** Menü ******");
+            Console.WriteLine("1 - Toplama");
+            Console.WriteLine("2 - Çıkartma");
+            Console.WriteLine("3 - Bölme");
+            Console.WriteLine("4 - Çarpma");
+            Console.Write("Lütfen değer seçiniz : ");
+        }
+
+        public void sonucEkranaYaz(decimal kullaniciSayi1, decimal kullaniciSayi2, decimal sonuc, string operators)
+        {
+            Console.WriteLine("{0} {1} {2} = {3}", kullaniciSayi1, operators, kullaniciSayi2, sonuc);
+        }
+    }
+}
---
+++ MatematikselIslemler/MatematikselIslemler/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatematikselIslemler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematik M = new Matematik();
+        YenidenIslemYap:
+            M.menuHazirla();
+            int kullaniciSecim = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen 1. Sayı değerini giriniz");
+            decimal kullaniciSayi1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen 2. Sayı değerini giriniz");
+            decimal kullaniciSayi2 = decimal.Parse(Console.ReadLine());
+
+            decimal sonuc = 0;
+
+            switch (kullaniciSecim)
+            {
+                case 1: // Toplama
+                    sonuc = M.toplamaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "+");
+                    break;
+                case 2: // Çıkartma
+                    sonuc = M.cıkartmaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "-");
+                    break;
+                case 3: // Bölme
+                    sonuc = M.bolmeIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "/");
+                    break;
+                case 4: // Çarpma
+                    sonuc = M.carpmaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "*");
+                    break;
+                default:
+                    Console.WriteLine("Belirtmiş Olduğunuz değer liste içerisinde bulunamadı");
+                    Console.WriteLine("Lütfen yeniden deneyiniz");
+                    System.Threading.Thread.Sleep(2000);
+                    goto YenidenIslemYap;
+
+            }
+            Console.WriteLine("Yeni İşlem Yapmak İstiyo musunuz ? [E/H] ");
+            string EH = Console.ReadLine();
+            if (EH == "E")
+            {
+                goto YenidenIslemYap;
+            }
+        }
+    }
+}
+
